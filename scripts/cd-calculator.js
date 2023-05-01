b06ca11d1b4b5af2df8e8570d4df4a94410d523e
A1CodeBreaker <129992490+A1CodeBreaker@users.noreply.github.com>
2023-05-01 14:49:57 -0400

    Completed Workshop1-Starter For Loan Calculator.

---
+++ scripts/cd-calculator.js
@@ -0,0 +1,27 @@
+console.log("Hello World!");
+window.onload = init;
+
+function init() {
+  console.log("init started");
+  const cdCalcBtnEl = document.getElementById("cdCalcBtn");
+  cdCalcBtnEl.onclick = oncdCalcBtnClicked;
+
+  console.log("init finished");
+}
+
+function oncdCalcBtnClicked() {
+  console.log("cdCalc Started");
+  const depositAmount = Number(document.getElementById("cdDeposit").value);
+  const cdRate = Number(document.getElementById("cdRate").value) / 100;
+  const cdTerm = Number(document.getElementById("cdTerm").value);
+  const maturityValue = depositAmount * (1 + cdRate / 365) ** (365 * cdTerm);
+  const interestAccrued = maturityValue - depositAmount;
+  document.getElementById("maturityValue").innerHTML =
+    "$" + maturityValue.toFixed(2);
+  document.getElementById("interestAccrued").innerHTML =
+    "$" + interestAccrued.toFixed(2);
+  console.log("cd Calc Finished");
+  // console.log("monthly interest rate: " + monthlyInterestRate);
+  // console.log("monthly Loan Term: " + monthlyLoanTerm);
+  //
+}
--- scripts/loan-calculator.js
+++ scripts/loan-calculator.js
@@ -1,1 +1,31 @@
-console.log('hello world!');
+console.log('Hello world!');
+window.onload = init;
+
+function init() {
+  console.log("init started");
+  const mortCalcBtnEl = document.getElementById("mortCalcBtn");
+  mortCalcBtnEl.onclick = onMortCalcBtnClicked;
+
+  console.log("init finished");
+}
+
+function onMortCalcBtnClicked() {
+  console.log("MortCalc Started");
+  const loanAmount = Number(document.getElementById("loanAmount").value);
+  const interestRate =
+    Number(document.getElementById("interestRate").value) / 100;
+  const monthlyInterestRate = interestRate / 12;
+  const loanTerm = Number(document.getElementById("loanTerm").value);
+  const monthlyLoanTerm = loanTerm * 12;
+  const monthlyPayment =
+    loanAmount *
+    (monthlyInterestRate / (1 - (1 + monthlyInterestRate) ** -monthlyLoanTerm));
+  const loanTotal = monthlyPayment * monthlyLoanTerm;
+  document.getElementById("monthlyPayment").innerHTML =
+    "$" + monthlyPayment.toFixed(2);
+  document.getElementById("totalCost").innerHTML = "$" + loanTotal.toFixed(2);
+  console.log("Mort Calc Finished");
+  //   console.log("monthly interest rate: " + monthlyInterestRate);
+  // console.log("monthly Loan Term: " + monthlyLoanTerm);
+  //
+}
